04aefa8105aa8a518d2ff3d91a9133657a3eac23
chak <851558146@qq.com>
2023-06-07 11:23:21 +0800

    hot-fix:文件HelloWorld.java的第1次提交

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld{
+
 	public static void main(String args[]) {
+	
 		System.out.println("Hello world!");
+		
+		System.out.println("hehe");
+		// 下面一行输出语句是在 hot-fix 分支上新增的
+		System.out.println("hot-fix: content");
 	}
 }
